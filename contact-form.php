b3f8f7a0c89d8d33fac8432f6ac9c5ce8ed34e44
Adam Pflantzer <apflantzer@gmail.com>
2014-05-09 13:54:13 -0400

    Use dummy google spreadsheet for contact form

--- contact-form.php
+++ contact-form.php
@@ -6,19 +6,19 @@
                 <h2>Contact <strong>Us</strong></h2>
             </div>
             <div class="modal-body">
-                <form action="https://docs.google.com/forms/d/........" method="POST" class="form-inline" role="form" id="contact-form" target="hidden_iframe" onsubmit="submitted = true">
+                <form action="https://docs.google.com/a/ogilvy.com/forms/d/1tg82o5v7nlN6TK77tBQwPu6B3gQOiVzgcUsu0pBT8l0/formResponse" method="POST" class="form-inline" role="form" id="contact-form" target="hidden_iframe" onsubmit="submitted = true">
                     <div class="form-group col-sm-6">
-                        <label class="sr-only" for="entry.1990127724">Name</label>
-                        <input type="text" name="entry.1990127724" class="form-control" id="entry.1990127724" placeholder="Your Name" required>
+                        <label class="sr-only" for="entry.1307659455">Name</label>
+                        <input type="text" name="entry.1307659455" class="form-control" id="entry.1307659455" placeholder="Your Name" required>
                         <span class="glyphicon glyphicon-asterisk"></span>
                     </div>
                     <div class="form-group col-sm-6">
-                        <label class="sr-only" for="entry.2001152739">Email</label>
-                        <input type="email" name="entry.2001152739" class="form-control" id="entry.2001152739" placeholder="Email Address" required>
+                        <label class="sr-only" for="entry.1739637305">Email</label>
+                        <input type="email" name="entry.1739637305" class="form-control" id="entry.1739637305" placeholder="Email Address" required>
                         <span class="glyphicon glyphicon-asterisk"></span>
                     </div>
                     <div class="form-group col-sm-12">
-                        <textarea class="form-control" rows="5" name="entry_665076604" id="entry_665076604" placeholder="Leave us a message here..."></textarea>
+                        <textarea class="form-control" rows="5" name="entry.1613435838" id="entry.1613435838" placeholder="Leave us a message here..."></textarea>
                     </div>
                     <div class="form-group col-sm-12">
                         <input type="submit" name="submit" class="btn btn-primary btn-block" id="form-submit" value="Submit">
